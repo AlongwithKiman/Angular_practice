69d0acd46850e23c3aab32e56e60568023f7660a
AlongwithKiman <expfunsce@snu.ac.kr>
2019-11-22 23:43:19 +0900

    start generating service

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+// 서버에서 이 정보를 받았다고 가정함.
+import {Hero} from './hero';            // Hero 클래스 쓰려고
+
+export const HEROES: Hero[] = [     // export : HEROES object 다른데다 줄 수 있도록.
+    { id: 11, name: 'Dr Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+  ];
